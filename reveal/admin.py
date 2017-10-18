d7adbe978cbda874094d64f350da8559930e81ed
F. Malina <fmalina@pm.me>
2017-10-18 14:50:46 +0100

    Add filtering.

--- reveal/admin.py
+++ reveal/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 class RevealAdmin(admin.ModelAdmin):
     list_display = ('enquiring', 'info_attr', 'created_at', 'content_type')
+    list_filter = ('info_attr', 'content_type')
 
 
 admin.site.register(Reveal, RevealAdmin)
